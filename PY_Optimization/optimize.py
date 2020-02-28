f3e1bb2527b67c2db947367bb56597c86b0fb2ca
njenning <njenn001@odu.edu>
2020-02-28 10:35:09 -0500

    Added input menu

--- PY_Optimization/optimize.py
+++ PY_Optimization/optimize.py
@@ -40,6 +40,49 @@ def eggHolderFunction(x,y):
 #############
 
 #############
+#Function to find the smallest value of all generated states 
+def findLowest(states, costs, xValues, yValues):
+
+    loop = 0
+    low = 0 
+    x = 0 
+    y = 0 
+    for s in states: 
+        loop += 1
+
+        #print("("+ str(s.x) + ", " + str(s.y) + ")", end = " ") 
+        costs.append(s.cost) 
+        xValues.append(s.x)
+        yValues.append(s.y) 
+
+        if (loop == 1):
+            #print ("Beginning") 
+            low = states[0].cost
+            x = states[0].x
+            y = states[0].y 
+        else: 
+            if (states[loop - 1].cost < low): 
+                low = states[loop - 1].cost
+                x = states[loop - 1].x
+                y = states[loop - 1].y
+
+                #print ("New lowest cost: " + str(low))
+
+    print ("Minimized Function State: \n")
+    print ("X: " + str(x))
+    print ("Y: " + str(y))
+    print ("Cost: " + str (low))
+#############
+
+#############
+#Function to implement Differential Evolution
+def dEvolution():
+    print ("Evolution")
+
+
+#############
+
+#############
 #Hill Climibing Optimization
 def HillClimbing(x, y, costs, states):
 
@@ -97,85 +140,98 @@ def HillClimbing(x, y, costs, states):
 
 #############
 #Main Function to display some information 
-def main():
+def main(): 
 
-    cost = 0 
-    costs = []  
-    states = [] 
+    print ("\nChoose a search pattern:")
+    print ("\n1) Hill Climbing \n2) Differential Evolution ")
 
-    i = 0 
-
-    while i < 100: 
-        i += 1 
-        #print ("\nrun: #" + str(i))
-
-        x_in = random.randint(-10000, 10000)
-        #print ("Initial X: " + str(x_in))
-        y_in = random.randint(-10000, 10000)
-        #print ("Initial Y: " + str(y_in))
-
-        cost = eggHolderFunction(x_in, y_in)
-        state = State(x_in, y_in, cost) 
-
-        #check = checkState(state, states)
-        
-        costs.append(cost)   
-        states.append(state)
-        
-        HillClimbing(x_in, y_in, costs, states)
-
-    print ("Finished")
-
-    costs = [] 
-    xValues = [] 
-    yValues = [] 
-
-    loop = 0
-    low = 0 
-    x = 0 
-    y = 0 
-    for s in states: 
-        loop += 1
-
-        #print("("+ str(s.x) + ", " + str(s.y) + ")", end = " ") 
-        costs.append(s.cost) 
-        xValues.append(s.x)
-        yValues.append(s.y) 
-
-        if (loop == 1):
-            #print ("Beginning") 
-            low = states[0].cost
-            x = states[0].x
-            y = states[0].y 
-        else: 
-            if (states[loop - 1].cost < low): 
-                low = states[loop - 1].cost
-                x = states[loop - 1].x
-                y = states[loop - 1].y
-
-                #print ("New lowest cost: " + str(low))
-
-    print ("Minimized Function State: \n")
-    print ("X: " + str(x))
-    print ("Y: " + str(y))
-    print ("Cost: " + str (low))
-
-    print (costs) 
-
-    fig = plt.figure() 
-    ax = plt.axes(projection='3d')
-    ax.scatter(xValues, yValues, costs, c = costs, cmap='viridis', linewidth=0.5)
-    #plt.show()
-    ax.set_title('Minima Distribution')
-    ax.plot_trisurf(xValues, yValues, costs, cmap='viridis', edgecolor='none')
-    ax.set_xlabel('$X values$', fontsize=10)
-    ax.set_ylabel('$Y values$', fontsize=10)
-    ax.set_zlabel('$EH(x,y)$', fontsize=10)
-    ax.zaxis.set_rotate_label(False) 
-
-    ax.yaxis._axinfo['label']['space_factor'] = 50.0
+    while True:
+        try:
+            choice = int (input("Choice: "))
+        except Exception as ex: 
+            print ("Not the correct input type")
+            continue
     
-    plt.show()
+        if (choice == 1): 
+
+            cost = 0 
+            costs = []  
+            states = [] 
+
+            i = 0
+            while i < 100: 
+                i += 1 
+                #print ("\nrun: #" + str(i))
+
+                x_in = random.randint(-10000, 10000)
+                #print ("Initial X: " + str(x_in))
+                y_in = random.randint(-10000, 10000)
+                #print ("Initial Y: " + str(y_in))
+
+                cost = eggHolderFunction(x_in, y_in)
+                state = State(x_in, y_in, cost) 
+
+                #check = checkState(state, states)
+                
+                costs.append(cost)   
+                states.append(state)
+                
+                HillClimbing(x_in, y_in, costs, states)
+
+            costs = [] 
+            xValues = [] 
+            yValues = [] 
+            
+            findLowest(states, costs, xValues, yValues)
+                
+                
+            fig = plt.figure() 
+
+            ax = plt.axes(projection='3d')
+            ax.scatter(xValues, yValues, costs, c = costs, cmap='viridis', linewidth=0.5)
+            
+            ax.set_title('Minima Distribution')
+            ax.plot_trisurf(xValues, yValues, costs, cmap='viridis', edgecolor='none')
+            ax.set_xlabel('$X values$', fontsize=10)
+            ax.set_ylabel('$Y values$', fontsize=10)
+            ax.set_zlabel('$EH(x,y)$', fontsize=10)
+            ax.zaxis.set_rotate_label(False) 
+
+            ax.yaxis._axinfo['label']['space_factor'] = 50.0
+
+            plt.show()
+
+            break
+        if (choice == 2): 
+            cost = 0 
+            costs = []  
+            states = [] 
+
+            i = 0
+            while i < 100: 
+                i += 1 
+
+                #print ("\nrun: #" + str(i))
+
+                x_in = random.randint(-10000, 10000)
+                #print ("Initial X: " + str(x_in))
+                y_in = random.randint(-10000, 10000)
+                #print ("Initial Y: " + str(y_in))
+
+                cost = eggHolderFunction(x_in, y_in)
+                state = State(x_in, y_in, cost) 
+
+                #check = checkState(state, states)
+                
+                costs.append(cost)   
+                states.append(state)
+
+                dEvolution() 
+
+
+            break
+
+    print ("Finished") 
 
 #############
     
